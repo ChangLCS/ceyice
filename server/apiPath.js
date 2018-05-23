84d12cc5cefb93ba0bac037941680a6fc9be4400
Chang <584911924@qq.com>
2018-05-23 23:20:05 +0800

    重新配置了项目路径，加了个server文件夹，那个是用来做node后台的，小程序项目移到wechat里面

---
+++ server/apiPath.js
@@ -0,0 +1,12 @@
+//  api地址相对应的方法路径
+
+module.exports = {
+  '/get/user': 'getUser',
+  '/set/user': 'setUser',
+  '/register': 'register',
+  '/order/create': 'orderCreate',
+  '/order/list': 'orderList',
+  '/order/other': 'orderOther',
+  '/order/list/accept': 'orderListAccept',
+  '/order/accept': 'orderAccept',
+};
